0ffd3b1946c5c94898ae966e62d4ef8e5a1e79b7
tayyabshabab <tayyabshabab@yahoo.com>
2013-11-14 13:55:44 +0100

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+	factory :user do
+		name										"Tayyab Shabab"
+		email										"tayyab@yahoo.com"
+		password								"test123"
+		password_confirmation		"test123"
+	end
+end
