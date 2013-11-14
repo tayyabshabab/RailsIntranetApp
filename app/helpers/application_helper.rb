66cc43a59a9ca8b7f2e41a052f69890b2afa6dc3
tayyabshabab <tayyabshabab@yahoo.com>
2013-11-14 11:01:39 +0100

    Finish layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+	# Returns the full title on a per-page basis.
+  def full_title(page_title)
+    base_title = "Intranet Application"
+    if page_title.empty?                              # Boolean test
+      base_title                                      # Implicit return
+    else
+      "#{base_title} | #{page_title}"                 # String interpolation
+    end
+  end
+
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,14 +7,29 @@ describe "StaticPages" do
 	describe "Homepage" do
 		before { visit root_path }
 
-		it { should have_content("IntranetApp") }
-		it { should have_title("IntranetApp") }
+		it { should have_content("Intranet Application") }
+		it { should have_title(full_title('')) }
+		it { should_not have_title("| Home")}
 	end
 
 	describe "Help page" do
-		before { visit root_path }
+		before { visit help_path }
 
 		it { should have_content("Help") }
-		it { should have_title("Help") }
+		it { should have_title(full_title("Help")) }
+	end
+
+	describe "About page" do
+		before { visit about_path }
+
+		it { should have_content("About") }
+		it { should have_title(full_title("About")) }
+	end
+
+	describe "Contact page" do
+		before { visit contact_path }
+
+		it { should have_content("Contact") }
+		it { should have_title(full_title("Contact")) }
 	end
 end
